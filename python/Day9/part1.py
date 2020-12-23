f339ec9fa69069882a14e12d81b32bb5c2dd0ff4
AD9000 <damle.atharv16@gmail.com>
2020-12-23 12:27:16 +1100

    completed day 9, created files upto day 20, removed init script from gitignore

--- python/Day9/part1.py
+++ python/Day9/part1.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+from sys import stdin
+from collections import deque, defaultdict
+
+sums = defaultdict(lambda: 0)
+# preamble
+nums = deque([int(input()) for i in range(25)])
+
+l = len(nums)
+
+for i in range(l):
+    for j in range(i + 1, l):
+        sums[nums[i] + nums[j]] += 1
+
+weak = -1
+for i in stdin:
+    i = int(i)
+    # check i
+    if sums[i] == 0:
+        weak = i
+        break
+    else:
+        lastNum = nums.popleft()
+        for j in nums:
+            prev = lastNum + j
+            sums[prev] -= 1
+            if sums[prev] == 0:
+                sums.pop(prev)
+
+            sums[i + j] += 1
+
+print(weak)
--- python/Day9/part2.py
+++ python/Day9/part2.py
@@ -1,1 +1,63 @@
 #!/usr/bin/env python3
+from sys import stdin
+from collections import deque, defaultdict
+
+sums = defaultdict(lambda: 0)
+# preamble
+nums = deque([int(input()) for i in range(25)])
+allNums = list(nums)
+
+l = len(nums)
+
+for i in range(l):
+    for j in range(i + 1, l):
+        sums[nums[i] + nums[j]] += 1
+
+weak = -1
+for i in stdin:
+    i = int(i)
+    allNums.append(i)
+    # check i
+    if sums[i] == 0:
+        weak = i
+        break
+    else:
+        lastNum = nums.popleft()
+        for j in nums:
+            sums[lastNum + j] -= 1
+            if sums[lastNum + j] == 0:
+                sums.pop(lastNum + j)
+            sums[i + j] += 1
+
+        nums.append(i)
+
+
+# contiguous sum -> sliding window
+lowest = 0
+s = allNums[lowest]
+highest = 1
+l = len(allNums)
+while highest < l:
+    print(s, weak, allNums[lowest], allNums[highest])
+    if s == weak:
+        print("done")
+        break
+    elif s > weak:
+        s -= allNums[lowest]
+        lowest += 1
+    else:
+        s += allNums[highest]
+        highest += 1
+
+# add check if last number was ever included
+
+# print(len(allNums))
+# print(allNums[lowest], allNums[highest - 1])
+
+print(lowest, highest)
+mn, mx = allNums[l - 1], 0
+for i in range(lowest, highest):
+    mn, mx = min(mn, allNums[i]), max(mx, allNums[i])
+
+
+print(mn, mx, mn + mx)
